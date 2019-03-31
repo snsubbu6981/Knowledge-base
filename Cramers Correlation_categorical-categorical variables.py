2cbbf72dc002cb15e741312b1514a9b6934afeef
Subbu Narayanaswamy <snsubbu@gmail.com>
2019-03-30 21:19:54 -0400

    Cramer's correlation between two categorical variables

---
+++ Cramers Correlation_categorical-categorical variables.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 21:17:51 2019
+
+@author: snarayanaswamy
+"""
+
+
+ """ calculate Cramers V statistic for categorial-categorial association.
+        uses correction from Bergsma and Wicher,
+        Journal of the Korean Statistical Society 42 (2013): 323-328
+    """
+    
+def cramers_v(x, y):
+    confusion_matrix = pd.crosstab(x,y)
+    chi2 = ss.chi2_contingency(confusion_matrix)[0]
+    n = confusion_matrix.sum().sum()
+    phi2 = chi2/n
+    r,k = confusion_matrix.shape
+    phi2corr = max(0, phi2-((k-1)*(r-1))/(n-1))
+    rcorr = r-((r-1)**2)/(n-1)
+    kcorr = k-((k-1)**2)/(n-1)
+    return np.sqrt(phi2corr/min((kcorr-1),(rcorr-1)))
+
+cramers_v(train['var_199'],train['target'])
